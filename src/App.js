3e96d37b76fc2d106b7ee48ae62d2aed97573083
Polina <p.moysenia@gmail.com>
2021-03-01 17:19:31 +0300

    Buttons added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { Button } from 'react-bootstrap';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Выбери настроение</p>
       </header>
+      <div className="App-body">
+        <Button className="custom-button" onClick={() => setState('happy')}>cчастливо</Button>
+        <Button className="custom-button" size="lg" onClick={() => setState('sad')}>грустно</Button>
+      </div>
     </div>
   );
+}
+
+function setState(state) {
+  axios('https://moodstateapi20210301143406.azurewebsites.net/state', {
+    method: 'post',
+    data: {
+      state: state
+    },
+  });
 }
 
 export default App;
